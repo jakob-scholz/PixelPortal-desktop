b25d4a47b80ae2532d1ee48effe1cb55fcebcaa8
Pharap <2933055+Pharap@users.noreply.github.com>
2022-03-14 17:54:08 +0000

    Replace dynamic allocation with static allocation

--- Level.cpp
+++ Level.cpp
@@ -6,10 +6,10 @@
 void Level::resetLevel()
 {
   for (int i = 0; i<12 ; i++){
-      this->portals[i] -> resetPortal();
+      this->portals[i].resetPortal();
   }
   for (int i = 0; i<12 ; i++){
-    this->walls[i] -> resetWall();
+    this->walls[i].resetWall();
   }
   level.keyTaken=false;
   level.openDoor = false;
@@ -36,10 +36,10 @@ void Level::loadLevel()
     this->door[0] = 100;
     this->door[1] = 22;
     
-    this->portals[0]->setPortal(110,55,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(10,25,PortalType::Normal,0,0,0,0);
+    this->portals[0].setPortal(110,55,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(10,25,PortalType::Normal,0,0,0,0);
     
-    this->walls[0]->setWall(0,30,128,2);
+    this->walls[0].setWall(0,30,128,2);
   }
   if(this->currentLevel==2){
     /*
@@ -60,10 +60,10 @@ void Level::loadLevel()
     this->door[0] = 5;
     this->door[1] = 56;
     
-    this->portals[0]->setPortal(110,55,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(10,25,PortalType::Normal,0,0,0,0);
+    this->portals[0].setPortal(110,55,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(10,25,PortalType::Normal,0,0,0,0);
     
-    this->walls[0]->setWall(0, 30, 128, 2);
+    this->walls[0].setWall(0, 30, 128, 2);
   }
   if(this->currentLevel==3){
     /*
@@ -84,11 +84,11 @@ void Level::loadLevel()
     this->key[0] = 49;
     this->key[1] = 30-7;
     
-    this->portals[0]->setPortal(80,55,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(80,20,PortalType::Normal,0,0,0,0);
+    this->portals[0].setPortal(80,55,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(80,20,PortalType::Normal,0,0,0,0);
 
-    this->walls[0]->setWall(104, 30, 12, 2);
-    this->walls[1]->setWall(44, 30, 12, 2);
+    this->walls[0].setWall(104, 30, 12, 2);
+    this->walls[1].setWall(44, 30, 12, 2);
   }
 
   if(this->currentLevel==4){
@@ -109,8 +109,8 @@ void Level::loadLevel()
     this->key[1] = 26;
     this->door[0] = 100;
     this->door[1] = 55;
-    this->portals[0]->setPortal(50,15,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(50,55,PortalType::Normal,0,0,0,0);
+    this->portals[0].setPortal(50,15,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(50,55,PortalType::Normal,0,0,0,0);
   }
   
   if(this->currentLevel==5){
@@ -131,12 +131,12 @@ void Level::loadLevel()
     this->key[1] = 25;
     this->door[0] = 115;
     this->door[1] = 15;
-    this->portals[0]->setPortal(95,15,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(20,15,PortalType::Normal,0,0,0,0);
+    this->portals[0].setPortal(95,15,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(20,15,PortalType::Normal,0,0,0,0);
 
-    this->walls[0]->setWall(100, 35, 12, 2);
-    this->walls[1]->setWall(75, 45, 12, 2);
-    this->walls[2]->setWall(50, 55, 12, 2);
+    this->walls[0].setWall(100, 35, 12, 2);
+    this->walls[1].setWall(75, 45, 12, 2);
+    this->walls[2].setWall(50, 55, 12, 2);
   }
   
   if(this->currentLevel==6){
@@ -158,11 +158,11 @@ void Level::loadLevel()
     this->door[0] = 115;
     this->door[1] = 55;
     
-    this->portals[0]->setPortal(74,45,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(10,12,PortalType::Normal,0,0,0,0);
+    this->portals[0].setPortal(74,45,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(10,12,PortalType::Normal,0,0,0,0);
     
-    this->walls[0]->setWall(0, 20, 20, 2);
-    this->walls[1]->setWall(20, 0, 2, 20);
+    this->walls[0].setWall(0, 20, 20, 2);
+    this->walls[1].setWall(20, 0, 2, 20);
   }
   if(this->currentLevel==7){
     /*
@@ -183,12 +183,12 @@ void Level::loadLevel()
     this->door[0] = 115;
     this->door[1] = 55;
     
-    this->portals[0]->setPortal(10,10,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(10,55,PortalType::Normal,0,0,0,0);
-    this->portals[2]->setPortal(40,55,PortalType::InvertV,3,0,0,0);
-    this->portals[3]->setPortal(74,55,PortalType::InvertV,2,0,0,0);
+    this->portals[0].setPortal(10,10,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(10,55,PortalType::Normal,0,0,0,0);
+    this->portals[2].setPortal(40,55,PortalType::InvertV,3,0,0,0);
+    this->portals[3].setPortal(74,55,PortalType::InvertV,2,0,0,0);
     
-    this->walls[1]->setWall(0, 20, 25, 2);
+    this->walls[1].setWall(0, 20, 25, 2);
   }
   if(this->currentLevel==8){
     /*
@@ -208,10 +208,10 @@ void Level::loadLevel()
     this->key[1] = 2;
     this->door[0] = 115;
     this->door[1] = 55;
-    this->portals[0]->setPortal(10,10,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(10,55,PortalType::Normal,0,0,0,0);
-    this->portals[2]->setPortal(30,55,PortalType::InvertV,3,0,0,0);
-    this->portals[3]->setPortal(74,55,PortalType::InvertV,2,0,0,0);
+    this->portals[0].setPortal(10,10,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(10,55,PortalType::Normal,0,0,0,0);
+    this->portals[2].setPortal(30,55,PortalType::InvertV,3,0,0,0);
+    this->portals[3].setPortal(74,55,PortalType::InvertV,2,0,0,0);
   }
   if(this->currentLevel==9){
     /*
@@ -231,15 +231,15 @@ void Level::loadLevel()
     this->key[1] = 2;
     this->door[0] = 115;
     this->door[1] = 55;
-    this->portals[0]->setPortal(10,10,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(10,55,PortalType::Normal,0,0,0,0);
-    this->portals[2]->setPortal(25,55,PortalType::InvertV,4,0,0,0);
-    this->portals[3]->setPortal(45,55,PortalType::InvertV,5,0,0,0);
-    this->portals[4]->setPortal(65,55,PortalType::InvertV,6,0,0,0);
-    this->portals[5]->setPortal(85,55,PortalType::InvertV,2,0,0,0);
-    this->portals[6]->setPortal(105,55,PortalType::InvertV,3,0,0,0);
+    this->portals[0].setPortal(10,10,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(10,55,PortalType::Normal,0,0,0,0);
+    this->portals[2].setPortal(25,55,PortalType::InvertV,4,0,0,0);
+    this->portals[3].setPortal(45,55,PortalType::InvertV,5,0,0,0);
+    this->portals[4].setPortal(65,55,PortalType::InvertV,6,0,0,0);
+    this->portals[5].setPortal(85,55,PortalType::InvertV,2,0,0,0);
+    this->portals[6].setPortal(105,55,PortalType::InvertV,3,0,0,0);
 
-    this->walls[1]->setWall(0, 20, 20, 2);
+    this->walls[1].setWall(0, 20, 20, 2);
   }
   if(this->currentLevel==10){
     /*
@@ -260,8 +260,8 @@ void Level::loadLevel()
     this->door[0] = 100;
     this->door[1] = 22;
 
-    this->portals[0]->setPortal(65,5,PortalType::Normal,1,1,20,0);
-    this->portals[1]->setPortal(10,55,PortalType::Normal,0,0,0,0);    
+    this->portals[0].setPortal(65,5,PortalType::Normal,1,1,20,0);
+    this->portals[1].setPortal(10,55,PortalType::Normal,0,0,0,0);    
     
   }
   if(this->currentLevel==11){
@@ -283,12 +283,12 @@ void Level::loadLevel()
     this->door[0] = 10;
     this->door[1] = 22;
 
-    this->portals[0]->setPortal(115,5,PortalType::Normal,1,1,40,40);
-    this->portals[1]->setPortal(10,55,PortalType::Normal,0,0,0,0);
-    this->portals[2]->setPortal(15,5,PortalType::Normal,3,1,40,0);
-    this->portals[3]->setPortal(100,55,PortalType::Normal,2,0,0,0);
+    this->portals[0].setPortal(115,5,PortalType::Normal,1,1,40,40);
+    this->portals[1].setPortal(10,55,PortalType::Normal,0,0,0,0);
+    this->portals[2].setPortal(15,5,PortalType::Normal,3,1,40,0);
+    this->portals[3].setPortal(100,55,PortalType::Normal,2,0,0,0);
 
-    this->walls[1]->setWall(64, 0, 2, 64);
+    this->walls[1].setWall(64, 0, 2, 64);
   }
   
   if(this->currentLevel==12){
@@ -310,21 +310,21 @@ void Level::loadLevel()
     this->door[0] = 50;
     this->door[1] = 25;
 
-    this->portals[0]->setPortal(10,45,PortalType::InvertV,2,0,0,0);
-    this->portals[1]->setPortal(30,45,PortalType::InvertV,2,0,0,0);
-    this->portals[2]->setPortal(50,45,PortalType::InvertV,3,0,0,0);
-    this->portals[3]->setPortal(75,45,PortalType::InvertV,0,0,0,0);
-    this->portals[4]->setPortal(95,45,PortalType::InvertV,0,0,0,0);
-    this->portals[5]->setPortal(115,45,PortalType::InvertV,6,0,0,0);
-    this->portals[6]->setPortal(10,15,PortalType::InvertV,9,0,0,0);
-    this->portals[7]->setPortal(30,15,PortalType::InvertV,1,0,0,0);
-    this->portals[8]->setPortal(50,15,PortalType::InvertV,0,0,0,0);
-    this->portals[9]->setPortal(75,15,PortalType::InvertV,11,0,0,0);
-    this->portals[10]->setPortal(95,15,PortalType::InvertV,11,0,0,0);
-    this->portals[11]->setPortal(115,15,PortalType::InvertV,0,0,0,0);
+    this->portals[0].setPortal(10,45,PortalType::InvertV,2,0,0,0);
+    this->portals[1].setPortal(30,45,PortalType::InvertV,2,0,0,0);
+    this->portals[2].setPortal(50,45,PortalType::InvertV,3,0,0,0);
+    this->portals[3].setPortal(75,45,PortalType::InvertV,0,0,0,0);
+    this->portals[4].setPortal(95,45,PortalType::InvertV,0,0,0,0);
+    this->portals[5].setPortal(115,45,PortalType::InvertV,6,0,0,0);
+    this->portals[6].setPortal(10,15,PortalType::InvertV,9,0,0,0);
+    this->portals[7].setPortal(30,15,PortalType::InvertV,1,0,0,0);
+    this->portals[8].setPortal(50,15,PortalType::InvertV,0,0,0,0);
+    this->portals[9].setPortal(75,15,PortalType::InvertV,11,0,0,0);
+    this->portals[10].setPortal(95,15,PortalType::InvertV,11,0,0,0);
+    this->portals[11].setPortal(115,15,PortalType::InvertV,0,0,0,0);
 
-    this->walls[0]->setWall(64, 0, 2, 64);
-    this->walls[1]->setWall(0,32 , 128, 2);
+    this->walls[0].setWall(64, 0, 2, 64);
+    this->walls[1].setWall(0,32 , 128, 2);
   }
   
   if(this->currentLevel==13){
@@ -346,16 +346,16 @@ void Level::loadLevel()
     this->door[0] = 120;
     this->door[1] = 1;
 
-    this->portals[0]->setPortal(10,55,PortalType::Normal,1,1,40,0);
-    this->portals[1]->setPortal(25,30,PortalType::Normal,2,1,40,15);
-    this->portals[2]->setPortal(50,10,PortalType::Normal,3,1,40,40);
-    this->portals[3]->setPortal(120,50,PortalType::Normal,0,0,0,0);
-    this->portals[4]->setPortal(70,55,PortalType::Normal,5,1,40,0);
-    this->portals[5]->setPortal(95,30,PortalType::Normal,6,1,40,15);
-    this->portals[6]->setPortal(110,15,PortalType::Normal,7,1,40,40);
-    this->portals[7]->setPortal(110,3,PortalType::Normal,0,0,0,0);
+    this->portals[0].setPortal(10,55,PortalType::Normal,1,1,40,0);
+    this->portals[1].setPortal(25,30,PortalType::Normal,2,1,40,15);
+    this->portals[2].setPortal(50,10,PortalType::Normal,3,1,40,40);
+    this->portals[3].setPortal(120,50,PortalType::Normal,0,0,0,0);
+    this->portals[4].setPortal(70,55,PortalType::Normal,5,1,40,0);
+    this->portals[5].setPortal(95,30,PortalType::Normal,6,1,40,15);
+    this->portals[6].setPortal(110,15,PortalType::Normal,7,1,40,40);
+    this->portals[7].setPortal(110,3,PortalType::Normal,0,0,0,0);
 
-    this->walls[1]->setWall(64, 0, 2, 64);
+    this->walls[1].setWall(64, 0, 2, 64);
   }
   if(this->currentLevel==14){
     player.x=16;
@@ -376,19 +376,19 @@ void Level::loadLevel()
      * |--------------------------------
      */
 
-    this->portals[0]->setPortal(8,55,PortalType::Normal,1,0,0,0);
-    this->portals[1]->setPortal(16,5,PortalType::Normal,0,0,0,0);
-    this->portals[2]->setPortal(24,55,PortalType::InvertV,2,0,0,0);
-    this->portals[3]->setPortal(40,55,PortalType::InvertV,4,0,0,0);
-    this->portals[4]->setPortal(52,55,PortalType::InvertV,0,0,0,0);
-    this->portals[5]->setPortal(70,55,PortalType::InvertV,0,0,0,0);
-    this->portals[6]->setPortal(82,55,PortalType::InvertV,5,0,0,0);
-    this->portals[7]->setPortal(104,55,PortalType::InvertV,8,0,0,0);
-    this->portals[8]->setPortal(120,55,PortalType::InvertV,0,0,0,0);
+    this->portals[0].setPortal(8,55,PortalType::Normal,1,0,0,0);
+    this->portals[1].setPortal(16,5,PortalType::Normal,0,0,0,0);
+    this->portals[2].setPortal(24,55,PortalType::InvertV,2,0,0,0);
+    this->portals[3].setPortal(40,55,PortalType::InvertV,4,0,0,0);
+    this->portals[4].setPortal(52,55,PortalType::InvertV,0,0,0,0);
+    this->portals[5].setPortal(70,55,PortalType::InvertV,0,0,0,0);
+    this->portals[6].setPortal(82,55,PortalType::InvertV,5,0,0,0);
+    this->portals[7].setPortal(104,55,PortalType::InvertV,8,0,0,0);
+    this->portals[8].setPortal(120,55,PortalType::InvertV,0,0,0,0);
 
-    this->walls[0]->setWall(30, 20, 2, 48);
-    this->walls[1]->setWall(60, 20, 2, 48);
-    this->walls[2]->setWall(90, 20, 2, 48);
+    this->walls[0].setWall(30, 20, 2, 48);
+    this->walls[1].setWall(60, 20, 2, 48);
+    this->walls[2].setWall(90, 20, 2, 48);
   }
 }
 
@@ -399,21 +399,21 @@ void Level::drawLevel(){
 
 void Level::drawElements(){
     for(int i = 0; i<12;i++){
-      if(this->portals[i]->motion==1){
-        this->portals[i]->wave=(this->portals[i]->wave+1)%(2*this->portals[i]->distance);
-        if(this->portals[i]->wave<this->portals[i]->distance){
-          this->portals[i]->x+=1;
+      if(this->portals[i].motion==1){
+        this->portals[i].wave=(this->portals[i].wave+1)%(2*this->portals[i].distance);
+        if(this->portals[i].wave<this->portals[i].distance){
+          this->portals[i].x+=1;
         }else{
-          this->portals[i]->x-=1;
+          this->portals[i].x-=1;
         }
         
       }
-      if(this->portals[i]->type==PortalType::Normal){
-        arduboy.drawRect(this->portals[i]->x-2, this->portals[i]->y-2, 4,4, WHITE);
+      if(this->portals[i].type==PortalType::Normal){
+        arduboy.drawRect(this->portals[i].x-2, this->portals[i].y-2, 4,4, WHITE);
       }
-      if(this->portals[i]->type==PortalType::InvertV){
-        arduboy.drawRect(this->portals[i]->x-2, this->portals[i]->y-2, 4,4, WHITE);
-        arduboy.fillRect(this->portals[i]->x-1, this->portals[i]->y-2, 2,4, BLACK);
+      if(this->portals[i].type==PortalType::InvertV){
+        arduboy.drawRect(this->portals[i].x-2, this->portals[i].y-2, 4,4, WHITE);
+        arduboy.fillRect(this->portals[i].x-1, this->portals[i].y-2, 2,4, BLACK);
       }
     }
   
@@ -436,7 +436,7 @@ void Level::drawWalls(){
   
   //Draw each wall
   for(int i = 0; i<12;i++){
-    arduboy.fillRect (this->walls[i]->x, this->walls[i]->y, this->walls[i]->w, this->walls[i]->h, WHITE);
+    arduboy.fillRect (this->walls[i].x, this->walls[i].y, this->walls[i].w, this->walls[i].h, WHITE);
   }
     
   //Draw screen Borders
